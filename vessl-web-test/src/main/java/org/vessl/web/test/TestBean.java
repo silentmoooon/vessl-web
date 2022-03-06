2560518bb9e739d0fdb5a103a2e4782289db678e
vessel <xie.can@outlook.com>
2022-03-06 22:47:08 +0800

    update

--- vessl-web-test/src/main/java/org/vessl/web/test/TestBean.java
+++
@@ -1,9 +0,0 @@
-package org.vessl.web.test;
-
-import lombok.Data;
-
-@Data
-public class TestBean {
-    private String name;
-    private int age;
-}
--- vessl-web-test/src/main/java/org/vessl/web/test/WebServiceInterface.java
+++
@@ -1,6 +0,0 @@
-package org.vessl.web.test;
-
-
-public interface WebServiceInterface {
-    public String getName();
-}
